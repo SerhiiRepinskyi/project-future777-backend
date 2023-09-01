97d564bfe1ab16a5aa73e36a91c723df6ce8d3f0
Vallyblink <alabama.toj@gmail.com>
2023-09-01 15:43:02 +0300

    adsad

--- helpers/CloudinaryConfig.js
+++ helpers/CloudinaryConfig.js
@@ -1,14 +1,15 @@
 import {v2 as cloudinary} from 'cloudinary';
 import "dotenv/config";
 
-const { CLOUDINARY_CLOUD_NAME,
+const { 
         CLOUDINARY_CLOUD_KEY,
         CLOUDINARY_CLOUD_SECRET } = process.env;
 
 cloudinary.config({
-    cloud_name: CLOUDINARY_CLOUD_NAME,
-    api_key:CLOUDINARY_CLOUD_KEY,
-    api_secret:CLOUDINARY_CLOUD_SECRET,
+    cloud_name: "teamprojectavatar",
+    api_key:"821778916524365",
+    api_secret: "ADb53ua2XFEy-4wSZeIBWN1KHW4",
+    API_base_URL:"https://api.cloudinary.com/v1_1/teamprojectavatar"
 })
 
 export default cloudinary;
